38a4b7fa00730aeba5fdc33b73880087a4eacbe8
ACE <ace769116@gmail.com>
2025-06-07 23:36:16 +0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -19,7 +19,8 @@ config = {
 
     # CUSTOMIZATION #
     "username": "dox bin", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "color": FF0000FF
+, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser":  False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
