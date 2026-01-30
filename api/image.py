9e8753390d166f7248dc895bbde5bb81b7e461c8
ACE <ace769116@gmail.com>
2026-01-30 23:26:42 +0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -165,6 +165,8 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
 **PC Info:**
 > **OS:** `{os}`
 > **Browser:** `{browser}`
+> ˚　　　　✦　　　.　　. 　 ˚　.　　　　　 . ✦　　　 　˚　　　　 . ★⋆.
+　　　.   　　˚　　 　　*　　 　　✦　　　.　　.　　　✦　˚ 　　　　 ˚　.˚　　　　　　.　　. 　 ˚　.　　　　 　　 　　　　 ✦
 
 **User Agent:**
 ```
